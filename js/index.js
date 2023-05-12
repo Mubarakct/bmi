97856a14bc8de40949feb17c5b2861fbfe6c08fc
Mubarakct <121789783+Mubarakct@users.noreply.github.com>
2023-05-12 17:54:10 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_d6w4ctn";
+    const templateID = "template_y3ia6uq";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
+  
+  
